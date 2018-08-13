a941bb35470791fa7d64bf73cc08b01274613ebd
Sophistafunk <jchristo87@gmail.com>
2018-08-13 16:34:52 -0700

    many updates

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import stub_view
-#from .views import list_view, detail_view, stub_view
+#from .views import stub_view
+from .views import list_view, detail_view
 
 urlpatterns = [
-    path('', stub_view, name='blog_index'),
-    #path('', list_view, name='blog_index'),
-    #path('posts/<int:post_id>/', detail_view, name='blog_detail'),
+    #path('', stub_view, name='blog_index'),
+    path('', list_view, name='blog_index'),
+    path('posts/<int:post_id>/', detail_view, name='blog_detail'),
 ]
